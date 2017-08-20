5e66868681da3d6cc8b886427b5d163ee955e0fd
Amol Athavale <ammira@Amols-MacBook-Air.local>
2017-08-20 17:09:49 +0100

    Initial commit

---
+++ src/main/java/com/ngco/springr/SpringrApplication.java
@@ -0,0 +1,12 @@
+package com.ngco.springr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrApplication.class, args);
+	}
+}
